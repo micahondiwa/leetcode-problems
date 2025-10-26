4437aca34266f39b92c79bf136fc544261b5b9e7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-10-26 13:27:09 +0300

    448-find_all_numbers_disappeared_in_an_arry

--- 0x00-arrays/448-find_all_numbers_disappeared_in_an_arry.py
+++ 0x00-arrays/448-find_all_numbers_disappeared_in_an_arry.py
@@ -1,4 +1,9 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        pass
-        
+        n = len(nums)
+        sets = set(nums)
+        dis = []
+        for i in set(i for i in range(1, n + 1)):
+            if i not in sets:
+                dis.append(i)
+        return dis
