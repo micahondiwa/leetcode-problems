5cea756db057a75fb4746c673456e52b2c5256f3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-10-26 14:33:06 +0300

    Two sum

---
+++ 0x00-arrays/1.two_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map ={}
+
+        for i, v in enumerate(nums):
+            if target - v in hash_map:
+                return i, hash_map[target - v]
+            else:
+                hash_map[v] = i
+        
