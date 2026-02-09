a2b1f2d4c3be2b060c11133aea346800deda5f95
micahondiwa <micah.oketch.ondiwa@gmail.com>
2026-02-09 08:32:45 +0300

    Is a valid palindrome

--- 0x01-arrays_two_pointers/0-isPalindrome.py
+++ 0x01-arrays_two_pointers/0-isPalindrome.py
@@ -1,16 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        cleaned = [char.lower() for char in s if char.isalnum()]
+def isPalindrome(s):
+    """
+    :type s: str
+     :rtype: bool
+     """
+    cleaned = [char.lower() for char in s if char.isalnum()]
 
-        
-        low, high = 0, len(cleaned)-1
-        while low <= high:
-            if cleaned[low] != cleaned[high]:
-                return False
-            low += 1
-            high -= 1
-        return True
+    low, high = 0, len(cleaned)-1
+    while low <= high:
+         if cleaned[low] != cleaned[high]:
+            return False
+         low += 1
+         high -= 1
+    return True
+# Example
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
