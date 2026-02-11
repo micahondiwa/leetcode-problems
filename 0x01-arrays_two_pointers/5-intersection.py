a3b1464174c83fcf2647c449c991f33ca3ff53a6
micahondiwa <micah.oketch.ondiwa@gmail.com>
2026-02-11 19:43:42 +0300

    Array intersection

---
+++ 0x01-arrays_two_pointers/5-intersection.py
@@ -0,0 +1,18 @@
+def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums1)
+        m = len(nums2)
+        res = []
+        
+        for i in range(n):
+            if nums1[i] in nums2 and nums1[i] not in res:
+                res.append(nums1[i])
+        for j in range(m):
+            if nums2[j] in nums1 and nums2[j] not in res:
+                res.append(nums2[j])
+        return res
+        
