801ee4c464055376d5d23099a50567548e95ca9b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2026-02-14 19:19:49 +0300

    Merge sorted Array

---
+++ 0x01-arrays_two_pointers/6-merge.py
@@ -0,0 +1,15 @@
+def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        for i in range(m):
+            for j in range(0, m+n-i-1):
+                if nums1[j] >= nums1[j+1]:
+                    nums1[j], nums1[j+1] = nums1[j+1], nums1[j]
